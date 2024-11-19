f8c9857e3573ba9b2be58ae205e73e8e7136f61d
afc163 <afc163@gmail.com>
2024-11-19 22:37:45 +0800

    first commit

---
+++ src/layouts/index.tsx
@@ -0,0 +1,21 @@
+import { Link, Outlet } from 'umi';
+import styles from './index.less';
+
+export default function Layout() {
+  return (
+    <div className={styles.navs}>
+      <ul>
+        <li>
+          <Link prefetch to="/">Home</Link>
+        </li>
+        <li>
+          <Link prefetch to="/docs">Docs</Link>
+        </li>
+        <li>
+          <a href="https://github.com/umijs/umi">Github</a>
+        </li>
+      </ul>
+      <Outlet />
+    </div>
+  );
+}
